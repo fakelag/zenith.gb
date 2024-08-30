384a10c631e9d0eb2633ae9a388890dae5d3a7d7
fakelag <35497506+fakelag@users.noreply.github.com>
2024-08-30 21:08:48 +0300

    impl opcode_rrc, rl, rr

--- src/cpu/inst_impl.rs
+++ src/cpu/inst_impl.rs
@@ -784,9 +784,27 @@ pub fn opcode_rlc(emu: &mut Emu, _instr: &Instruction, opcode: u8) {
     cpu::CPU::write_r8(emu, reg, new_val);
 }
 
-pub fn opcode_rrc(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F"); }
-pub fn opcode_rl(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17"); }
-pub fn opcode_rr(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F"); }
+pub fn opcode_rrc(emu: &mut Emu, _instr: &Instruction, opcode: u8) {
+    let reg = opcode & 0x7;
+    let val = cpu::CPU::read_r8(emu, reg);
+    let new_val = rrc(emu, val);
+    cpu::CPU::write_r8(emu, reg, new_val);
+}
+
+pub fn opcode_rl(emu: &mut Emu, _instr: &Instruction, opcode: u8) {
+    let reg = opcode & 0x7;
+    let val = cpu::CPU::read_r8(emu, reg);
+    let new_val = rl(emu, val);
+    cpu::CPU::write_r8(emu, reg, new_val);
+}
+
+pub fn opcode_rr(emu: &mut Emu, _instr: &Instruction, opcode: u8) {
+    let reg = opcode & 0x7;
+    let val = cpu::CPU::read_r8(emu, reg);
+    let new_val = rr(emu, val);
+    cpu::CPU::write_r8(emu, reg, new_val);
+}
+
 pub fn opcode_sla(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27"); }
 pub fn opcode_sra(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F"); }
 pub fn opcode_swap(_emu: &mut Emu, _instr: &Instruction, _opcode: u8) { todo!("0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37"); }
